8d12c547df3a422a8d1993725cf4e30ef65725ba
Anna <annaange55@gmail.com>
2021-12-28 20:25:45 +0200

    smth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route } from 'react-router-dom';
 import './App.scss';
+import Home from './components/Home';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
-      fucking pizza-maker
+      <Route exact path={['/', '/home']} render={() => <Home />} />
+      <Route path="/main" render={() => <Main />} />
     </div>
   );
 }
---
+++ src/components/main.jsx
@@ -0,0 +1,7 @@
+const Main = ()=>{
+    return(
+        <div>main part</div>
+    )
+}
+
+export default Main
